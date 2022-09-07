0ac29920398c8bed70863ba6a68181c281bc0633
Xenoxil <xilus1127@gmail.com>
2022-09-07 21:27:17 +0300

    настроить запрос полной статистики

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -9,80 +9,89 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoutes';
 function App() {
   const [loggedInState, setLoggedIn] = useState(false);
   const [user, setUser] = useState('');
-  const [stats, setStats] = useState([]);
+  const [stats, setStats] = useState<any[]>([]);
   const navigate = useNavigate();
-  const [token,setToken]=useState('');
-  const [shortLink,setShortLink]=useState('');  
+  const [token, setToken] = useState('');
+  const [shortLink, setShortLink] = useState('');
   const [notificationVisibility, setNotificationVisibility] = useState(false);
-  const [notificationMessage, setNotificationMessage] = useState(''); 
+  const [notificationMessage, setNotificationMessage] = useState('');
   const [buttonDisableState, setButtonDisableState] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  const [middleResult,setMiddleResult] = useState([]);
-  const [renderedLinks, setRenderedLinks] = useState([]);
+  const [middleResult, setMiddleResult] = useState<any[]>([]);
+  const [renderedLinks, setRenderedLinks] = useState<any[]>([]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const [linksPerPage] = useState(10);
-  const [pages,setPages]=useState<number[]>([]);
+  const [pages, setPages] = useState<number[]>([]);
 
   const lastLinkIndex = currentPage * linksPerPage;
-  const firstLinkIndex = lastLinkIndex - linksPerPage;  
+  const firstLinkIndex = lastLinkIndex - linksPerPage;
 
-  function paginate(pageNumber:number){
+  function paginate(pageNumber: number) {
     setCurrentPage(pageNumber);
   }
 
-
   React.useEffect(() => {
-    document.title = `UrlShortener`;    
-    if(localStorage.getItem('access_token')){
+    document.title = `UrlShortener`;
+    if (localStorage.getItem('access_token')) {
       setUser(localStorage.getItem('userEmail') ?? '');
-      setToken(localStorage.getItem('access_token') ?? '')
+      setToken(localStorage.getItem('access_token') ?? '');
       setLoggedIn(true);
       navigate('/');
       setIsLoading(true);
-      mainApi.getStatistics((localStorage.getItem('access_token') ?? ''),1,500)
-    .then((stats)=>{
-      setStats(stats);
-      setMiddleResult(stats);
-      setRenderedLinks(stats.slice(firstLinkIndex,lastLinkIndex))
-      setIsLoading(false);      
-    })
-      .catch((err) => console.log('Ошибка:', err));
-  }
-}, [user]);
+      mainApi
+        .getStatistics(localStorage.getItem('access_token') ?? '', 1, 500)
+        .then((stats) => {
+          setStats(stats);
+          setMiddleResult(stats);
+          setRenderedLinks(stats.slice(firstLinkIndex, lastLinkIndex));
+          setIsLoading(false);
+          mainApi
+            .getStatistics(localStorage.getItem('access_token') ?? '', 500, 5000)
+            .then((res: any) => {
+              console.log(res);
+              console.log(stats.concat(res));
+              setStats(stats.concat(res));
+              setMiddleResult(stats.concat(res));              
+            })
+            .catch((err) => console.log('Ошибка:', err));
+        })
+        .catch((err) => console.log('Ошибка:', err));
+    }
+  }, []);
 
-  useEffect(()=>{
-    setRenderedLinks(middleResult.slice(firstLinkIndex,lastLinkIndex))
-  },[currentPage,middleResult])
+  useEffect(() => {
+    setRenderedLinks(middleResult.slice(firstLinkIndex, lastLinkIndex));
+  }, [currentPage, middleResult]);
 
-  useEffect(()=>{
+  useEffect(() => {
     const pageNumbers = [];
     for (let i = 1; i <= Math.ceil(middleResult.length / linksPerPage); i++) {
-        pageNumbers.push(i);
+      pageNumbers.push(i);
     }
     setPages(pageNumbers);
-  },[middleResult])
+  }, [middleResult]);
 
   function handleLoginClick(email: string, password: string) {
     setButtonDisableState(true);
     mainApi
       .login(email, password)
-      .then((res) => {        
-        handlingNotification('Successfull log in')
-        localStorage.setItem('userEmail',email);
-        localStorage.setItem('access_token',res.access_token);
-        localStorage.setItem('token_type',res.token_type);
+      .then((res) => {
+        handlingNotification('Successfull log in');
+        localStorage.setItem('userEmail', email);
+        localStorage.setItem('access_token', res.access_token);
+        localStorage.setItem('token_type', res.token_type);
         setUser(email);
         setLoggedIn(true);
         navigate('/');
       })
       .catch((err) => {
-        handlingNotification('Error'+err);
+        handlingNotification('Error' + err);
         console.log('Ошибка:' + err);
       })
-      .finally(()=>{
+      .finally(() => {
         setButtonDisableState(false);
-      })
+      });
   }
 
   function handleRegisterClick(email: string, password: string) {
@@ -90,74 +99,75 @@ function App() {
     mainApi
       .register(email, password)
       .then((res) => {
-        handlingNotification('Successfull register')
+        handlingNotification('Successfull register');
         handleLoginClick(email, password);
       })
       .catch((err) => {
-        handlingNotification('Error'+err);
+        handlingNotification('Error' + err);
         console.log('Ошибка:' + err);
       })
-      .finally(()=>{
+      .finally(() => {
         setButtonDisableState(false);
-      })
+      });
   }
 
-  function handleLogoutClick(){
+  function handleLogoutClick() {
     localStorage.clear();
     setLoggedIn(false);
-    handlingNotification('Successfull log out')
+    handlingNotification('Successfull log out');
   }
 
-  function squeezeLink(link:string){    
+  function squeezeLink(link: string) {
     setButtonDisableState(true);
-     mainApi.getSqueeze(link,token)
-     .then((res:any)=>{      
-        setShortLink(`http://79.143.31.216/s/${res.short}`)
-        handlingNotification('Link has been squeezed');        
-     })
-     .catch((err)=>{      
-      if(err==='422'){
-        handlingNotification('Error:422. Link should be valid URL'); 
-      }
-      else{
-        handlingNotification('Error: '+err);        
-      }
-      
-     })
-     .finally(()=>{
-      setButtonDisableState(false);
-     })
+    mainApi
+      .getSqueeze(link, token)
+      .then((res: any) => {
+        setShortLink(`http://79.143.31.216/s/${res.short}`);
+        handlingNotification('Link has been squeezed');
+        setStats([res,...stats]);
+        setMiddleResult([res,...stats]);
+      })
+      .catch((err) => {
+        if (err === '422') {
+          handlingNotification('Error:422. Link should be valid URL');
+        } else {
+          handlingNotification('Error: ' + err);
+        }
+      })
+      .finally(() => {
+        setButtonDisableState(false);
+      });
   }
 
-   function handleFiltering(ID:number,link:string,countFilter:string){
+  function handleFiltering(ID: number, link: string, countFilter: string) {
     setButtonDisableState(true);
-      let filteredStats=stats;      
-          if(ID>0){
-            filteredStats=stats.filter((linkObject:any)=>{
-            return linkObject.id.toString().includes(ID)
-          })}
-          if(link.length>0){
-            filteredStats=filteredStats.filter((linkObject:any)=>{
-              return linkObject.target.includes(link)
-            })
-          }
-          if(countFilter==='ASC'){
-            filteredStats.sort((a:any,b:any)=>{
-              return a.counter-b.counter
-            })          
-          }
-          else if(countFilter==='DESC'){
-            filteredStats.sort((a:any,b:any)=>{
-              return b.counter-a.counter
-            })   
-          }          
-            setMiddleResult(filteredStats);
-            handlingNotification('Successful filtering')
-            setCurrentPage(1);
-            setButtonDisableState(false);
-   }
+    let filteredStats = stats;
+    if (ID > 0) {
+      filteredStats = stats.filter((linkObject: any) => {
+        return linkObject.id.toString().includes(ID);
+      });
+    }
+    if (link.length > 0) {
+      filteredStats = filteredStats.filter((linkObject: any) => {
+        return linkObject.target.includes(link);
+      });
+    }
+    if (countFilter === 'ASC') {
+      filteredStats.sort((a: any, b: any) => {
+        return a.counter - b.counter;
+      });
+    } else if (countFilter === 'DESC') {
+      filteredStats.sort((a: any, b: any) => {
+        return b.counter - a.counter;
+      });
+    }
+    setMiddleResult(filteredStats);
+    handlingNotification('Successful filtering');
+    setCurrentPage(1);
+    setButtonDisableState(false);
+  }
 
-   function handlingNotification(notificationMessage:string) {    
+  function handlingNotification(notificationMessage: string) {
     setNotificationMessage(notificationMessage);
     setNotificationVisibility(true);
     setTimeout(() => {
@@ -168,32 +178,44 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/reg-log" element={<Reglog onRegister={handleRegisterClick} onLogin={handleLoginClick}
-         handlingNotification={handlingNotification}
-         isNotificationVisible={notificationVisibility}
-            notificationMessage={notificationMessage}
-            buttonDisableState={buttonDisableState}/>}
-             />
+        <Route
+          path="/reg-log"
+          element={
+            <Reglog
+              onRegister={handleRegisterClick}
+              onLogin={handleLoginClick}
+              handlingNotification={handlingNotification}
+              isNotificationVisible={notificationVisibility}
+              notificationMessage={notificationMessage}
+              buttonDisableState={buttonDisableState}
+            />
+          }
+        />
         <Route element={<ProtectedRoute loggedIn={loggedInState} />}>
-          <Route path="/" element={<Main
-          userEmail={user}
-          links={stats}
-          shortLink={shortLink}
-          squeeze={squeezeLink}
-          logOut={handleLogoutClick}
-          filtering={handleFiltering}
-          renderedLinks={renderedLinks}
-          handlingNotification={handlingNotification}
-          isNotificationVisible={notificationVisibility}
-            notificationMessage={notificationMessage}
-            buttonDisableState={buttonDisableState}
-            isLoading={isLoading}
-            paginate={paginate}
-            linksPerPage={linksPerPage}
-            currentPage={currentPage}
-            pages={pages}
-            setPages={setPages}
-             />} />
+          <Route
+            path="/"
+            element={
+              <Main
+                userEmail={user}
+                links={stats}
+                shortLink={shortLink}
+                squeeze={squeezeLink}
+                logOut={handleLogoutClick}
+                filtering={handleFiltering}
+                renderedLinks={renderedLinks}
+                handlingNotification={handlingNotification}
+                isNotificationVisible={notificationVisibility}
+                notificationMessage={notificationMessage}
+                buttonDisableState={buttonDisableState}
+                isLoading={isLoading}
+                paginate={paginate}
+                linksPerPage={linksPerPage}
+                currentPage={currentPage}
+                pages={pages}
+                setPages={setPages}
+              />
+            }
+          />
         </Route>
       </Routes>
     </div>
